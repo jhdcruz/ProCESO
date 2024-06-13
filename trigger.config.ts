32558420845aade949a32d0a333274af1b952374
jhdcruz <jhdcruz@proton.me>
2024-06-14 00:03:31 +0800

    chore: add trigger.dev integration

---
+++ trigger.config.ts
@@ -0,0 +1,16 @@
+import type { TriggerConfig } from '@trigger.dev/sdk/v3'
+
+export const config: TriggerConfig = {
+  project: 'proj_gvzufybtsolqvrrqdcte',
+  logLevel: 'log',
+  retries: {
+    enabledInDev: true,
+    default: {
+      maxAttempts: 2,
+      minTimeoutInMs: 1000,
+      maxTimeoutInMs: 15000,
+      factor: 2,
+      randomize: true,
+    },
+  },
+}
