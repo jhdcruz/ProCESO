50453feec1aeb455ca7e3d5a134a0ff9ed0ac0b3
jhdcruz <jhdcruz@proton.me>
2024-06-15 01:58:18 +0800

    fix(utils): misnamed supabase function

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,23 +1,21 @@
 import {
   type CookieOptions,
   createServerClient as supaServerClient,
-} from '@supabase/ssr'
-import { cookies } from 'next/headers'
+} from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
-export const createServiceClient = (
-  cookieStore: ReturnType<typeof cookies>,
-) => {
+export const createServerClient = (cookieStore: ReturnType<typeof cookies>) => {
   return supaServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookieStore.set({ name, value, ...options });
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -26,7 +24,7 @@ export const createServiceClient = (
         },
         remove(name: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value: '', ...options })
+            cookieStore.set({ name, value: '', ...options });
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -35,5 +33,5 @@ export const createServiceClient = (
         },
       },
     },
-  )
-}
+  );
+};
