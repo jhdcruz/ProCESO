b2ea5e65d3d327b207dd1d47ac5f6eef59cabe9b
jhdcruz <jhdcruz@proton.me>
2024-06-16 14:11:21 +0800

    fix(login); add page loader

---
+++ src/app/login/loading.tsx
@@ -0,0 +1,24 @@
+import { Paper, Stack, Text } from '@mantine/core';
+import Image from 'next/image';
+import cesoLogo from './ceso-manila.webp';
+
+export default function Loading() {
+  return (
+    <Paper className="flex h-screen w-screen items-center self-center align-middle">
+      <Stack className="mx-auto text-center">
+        <Image
+          className="rounded-md"
+          src={cesoLogo}
+          alt="Loading"
+          draggable={false}
+          placeholder="blur"
+          width={300}
+          height={130}
+          priority
+        />
+
+        <Text>We're preparing the system, please wait...</Text>
+      </Stack>
+    </Paper>
+  );
+}
