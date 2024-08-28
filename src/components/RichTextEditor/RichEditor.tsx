95c05bef73eaed5fbae316efa627f0e1201b18d1
jhdcruz <jhdcruz@proton.me>
2024-08-28 23:49:55 +0800

    feat: rich text editor base component

---
+++ src/components/RichTextEditor/RichEditor.tsx
@@ -0,0 +1,129 @@
+import { memo, useEffect } from 'react';
+import { useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import { Link, RichTextEditor } from '@mantine/tiptap';
+import ColorPicker from '@tiptap/extension-color';
+import TextStyle from '@tiptap/extension-text-style';
+import Superscript from '@tiptap/extension-superscript';
+import SubScript from '@tiptap/extension-subscript';
+import Highlight from '@tiptap/extension-highlight';
+import TextAlign from '@tiptap/extension-text-align';
+import '@mantine/tiptap/styles.css';
+
+/**
+ * Display an editable rich text editor.
+ *
+ * @param editable - Allow edits. (`false` by default)
+ * @param content - HTML content to render.
+ */
+function RichEditor({
+  editable,
+  content,
+}: {
+  editable: boolean;
+  content: string | undefined | null;
+}) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      TextStyle,
+      ColorPicker.configure({
+        types: ['heading', 'paragraph'],
+      }),
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content:
+      content ??
+      '<p>This event has no description yet. Come back again later.</p>',
+    editable: editable,
+    immediatelyRender: false,
+  });
+
+  // Set the editor to be editable or not
+  useEffect(() => {
+    if (editor) {
+      editor.setEditable(editable);
+    }
+  }, [editable, editor]);
+
+  return (
+    <RichTextEditor editor={editor}>
+      {editor && editable && (
+        <RichTextEditor.Toolbar sticky stickyOffset={60}>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.ColorPicker
+              colors={[
+                '#25262b',
+                '#868e96',
+                '#fa5252',
+                '#e64980',
+                '#be4bdb',
+                '#7950f2',
+                '#4c6ef5',
+                '#228be6',
+                '#15aabf',
+                '#12b886',
+                '#40c057',
+                '#82c91e',
+                '#fab005',
+                '#fd7e14',
+              ]}
+            />
+            <RichTextEditor.UnsetColor />
+          </RichTextEditor.ControlsGroup>
+
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Bold />
+            <RichTextEditor.Italic />
+            <RichTextEditor.Underline />
+            <RichTextEditor.Strikethrough />
+            <RichTextEditor.ClearFormatting />
+            <RichTextEditor.Highlight />
+          </RichTextEditor.ControlsGroup>
+
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Link />
+            <RichTextEditor.Unlink />
+          </RichTextEditor.ControlsGroup>
+
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.AlignLeft />
+            <RichTextEditor.AlignCenter />
+            <RichTextEditor.AlignJustify />
+            <RichTextEditor.AlignRight />
+          </RichTextEditor.ControlsGroup>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.H1 />
+            <RichTextEditor.H2 />
+            <RichTextEditor.H3 />
+            <RichTextEditor.H4 />
+          </RichTextEditor.ControlsGroup>
+
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Blockquote />
+            <RichTextEditor.Hr />
+            <RichTextEditor.BulletList />
+            <RichTextEditor.OrderedList />
+            <RichTextEditor.Subscript />
+            <RichTextEditor.Superscript />
+          </RichTextEditor.ControlsGroup>
+
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Undo />
+            <RichTextEditor.Redo />
+          </RichTextEditor.ControlsGroup>
+        </RichTextEditor.Toolbar>
+      )}
+
+      <RichTextEditor.Content />
+    </RichTextEditor>
+  );
+}
+
+export default memo(RichEditor);
