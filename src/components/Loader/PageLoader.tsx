2d45632d451cf6f1b8300976764f0ab4e88d71d2
jhdcruz <jhdcruz@proton.me>
2024-08-31 01:28:35 +0800

    fix: page loader going beyond screen

--- src/components/Loader/PageLoader.tsx
+++ src/components/Loader/PageLoader.tsx
@@ -2,7 +2,7 @@ import { rem, Paper, Stack, Text } from '@mantine/core';
 import { IconFidgetSpinner } from '@tabler/icons-react';
 
 export const PageLoader = () => (
-  <Paper className="flex h-screen w-screen items-center self-center align-middle">
+  <Paper className="h-100 w-100 flex items-center self-center align-middle">
     <Stack className="mx-auto text-center">
       <IconFidgetSpinner
         className="var(--mantine-color-dimmed) mx-auto block h-10 w-10 animate-spin"
