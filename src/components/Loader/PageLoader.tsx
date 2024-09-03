2c13a2d2f885b567e3a0be59179d2d177c0af891
jhdcruz <jhdcruz@proton.me>
2024-09-03 17:19:41 +0800

    style: use size instead of width and height

--- src/components/Loader/PageLoader.tsx
+++ src/components/Loader/PageLoader.tsx
@@ -1,9 +1,9 @@
-import { rem, Paper, Stack, Text } from '@mantine/core';
+import { Paper, Stack, Text } from '@mantine/core';
 import { IconFidgetSpinner } from '@tabler/icons-react';
 
 export const PageLoader = () => (
-  <Paper className="flex h-full w-full items-center self-center align-middle">
-    <Stack className="mx-auto text-center">
+  <Paper className="flex size-full items-center self-center py-10 align-middle">
+    <Stack className="mx-auto size-full text-center">
       <IconFidgetSpinner
         className="var(--mantine-color-dimmed) mx-auto block h-10 w-10 animate-spin"
         stroke={1.5}
