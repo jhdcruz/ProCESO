ff2d30a0bd740d14c0feae61c8025339b8dd69cf
jhdcruz <jhdcruz@proton.me>
2024-09-04 00:25:18 +0800

    feat: skip login on existing session

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
 import { lazy, Suspense } from 'react';
 import type { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 import Image from 'next/image';
 import { metadata as defaultMetadata } from '@/app/layout';
+import { getCurrentUser } from '@/api/supabase/user';
 import { JitbitScript } from '@/components/Integrations/JitbitScript';
 import loginBg from '@/components/_assets/img/login-bg.webp';
 import '@/styles/jitbit.css';
@@ -17,7 +19,13 @@ export const metadata: Metadata = {
   description: defaultMetadata.description,
 };
 
-export default function Login() {
+export default async function Login() {
+  // skip login page for logged-in users
+  const user = await getCurrentUser();
+  if (user.status === 0) {
+    redirect('/');
+  }
+
   return (
     <div className="z-10 min-h-screen max-w-full md:flex">
       <Suspense>
