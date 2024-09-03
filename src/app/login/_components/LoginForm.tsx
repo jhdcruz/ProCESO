8b7c8d7c6419a569ef6f9324dcd17f843e8383b6
jhdcruz <jhdcruz@proton.me>
2024-09-04 00:22:35 +0800

    perf: lazy import login form

---
+++ src/app/login/_components/LoginForm.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { memo } from 'react';
+import {
+  Button,
+  Divider,
+  Group,
+  Paper,
+  PasswordInput,
+  Space,
+  Stack,
+  TextInput,
+} from '@mantine/core';
+import Image from 'next/image';
+import { ThemeSwitcher } from '@/components/Buttons/ThemeSwitcher';
+import cesoLogo from '@/components/_assets/img/ceso-manila.webp';
+
+import { onEmailLogin } from '../actions';
+import { SystemHealth } from './SystemHealth';
+import { GoogleButton } from './GoogleButton';
+
+export function LoginFormComponent() {
+  return (
+    <form action={onEmailLogin} className="min-h-screen max-w-full md:flex">
+      <Paper
+        className="z-10 flex min-h-screen max-w-full flex-col items-center justify-between pt-10 md:w-[28rem] md:max-w-md"
+        p={30}
+        radius={0}
+        shadow="xl"
+      >
+        <main className="h-max w-[90%]">
+          <Stack align="stretch" gap="sm" justify="center" mb={20} mt={30}>
+            <Image
+              alt=""
+              className="mx-auto mb-4 rounded-[--mantine-radius-md] bg-contain shadow-lg"
+              draggable={false}
+              height={104}
+              placeholder="blur"
+              src={cesoLogo}
+              width={260}
+            />
+          </Stack>
+
+          <TextInput
+            autoCapitalize="none"
+            autoComplete="email"
+            autoCorrect="off"
+            label="Email address"
+            name="email"
+            placeholder="admin@tip.edu.ph"
+          />
+          <PasswordInput
+            autoCapitalize="none"
+            autoCorrect="off"
+            label="Password"
+            mt="md"
+            name="password"
+            placeholder="Your password"
+          />
+          <Button fullWidth mt="xl" type="submit">
+            Login
+          </Button>
+
+          <Divider label="Or continue using" labelPosition="center" my="lg" />
+
+          <GoogleButton />
+        </main>
+
+        <Group className="w-full" justify="space-around">
+          <ThemeSwitcher />
+          <SystemHealth />
+          <Space w="xs" />
+          <Space w="xm" />
+        </Group>
+      </Paper>
+    </form>
+  );
+}
+
+export const LoginForm = memo(LoginFormComponent);
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,31 +1,17 @@
+import { lazy, Suspense } from 'react';
 import type { Metadata } from 'next';
-import {
-  Button,
-  Divider,
-  Group,
-  Paper,
-  PasswordInput,
-  Space,
-  Stack,
-  TextInput,
-} from '@mantine/core';
 import Image from 'next/image';
-
-import { ThemeSwitcher } from '@/components/Buttons/ThemeSwitcher';
 import { metadata as defaultMetadata } from '@/app/layout';
 import { JitbitScript } from '@/components/Integrations/JitbitScript';
-
-import { onEmailLogin } from './actions';
-import { SystemHealth } from './_components/SystemHealth';
-import { GoogleButton } from './_components/GoogleButton';
-
-import cesoLogo from '@/components/_assets/img/ceso-manila.webp';
 import loginBg from '@/components/_assets/img/login-bg.webp';
 import '@/styles/jitbit.css';
 
-/**
- * Site metadata
- */
+const LoginForm = lazy(() =>
+  import('@/app/login/_components/LoginForm').then((mod) => ({
+    default: mod.LoginForm,
+  })),
+);
+
 export const metadata: Metadata = {
   title: 'Login - ' + defaultMetadata.title,
   description: defaultMetadata.description,
@@ -33,58 +19,10 @@ export const metadata: Metadata = {
 
 export default function Login() {
   return (
-    <form action={onEmailLogin} className="min-h-screen max-w-full md:flex">
-      <Paper
-        className="z-10 flex min-h-screen max-w-full flex-col items-center justify-between pt-10 md:w-[28rem] md:max-w-md"
-        p={30}
-        radius={0}
-        shadow="xl"
-      >
-        <main className="h-max w-[90%]">
-          <Stack align="stretch" gap="sm" justify="center" mb={20} mt={30}>
-            <Image
-              alt=""
-              className="mx-auto mb-4 rounded-[--mantine-radius-md] bg-contain shadow-lg"
-              draggable={false}
-              height={104}
-              placeholder="blur"
-              src={cesoLogo}
-              width={260}
-            />
-          </Stack>
-
-          <TextInput
-            autoCapitalize="none"
-            autoComplete="email"
-            autoCorrect="off"
-            label="Email address"
-            name="email"
-            placeholder="admin@tip.edu.ph"
-          />
-          <PasswordInput
-            autoCapitalize="none"
-            autoCorrect="off"
-            label="Password"
-            mt="md"
-            name="password"
-            placeholder="Your password"
-          />
-          <Button fullWidth mt="xl" type="submit">
-            Login
-          </Button>
-
-          <Divider label="Or continue using" labelPosition="center" my="lg" />
-
-          <GoogleButton />
-        </main>
-
-        <Group className="w-full" justify="space-around">
-          <ThemeSwitcher />
-          <SystemHealth />
-          <Space w="xs" />
-          <Space w="xm" />
-        </Group>
-      </Paper>
+    <div className="z-10 min-h-screen max-w-full md:flex">
+      <Suspense>
+        <LoginForm />
+      </Suspense>
 
       {/* Image Wall */}
       <Image
@@ -97,6 +35,6 @@ export default function Login() {
       />
 
       <JitbitScript />
-    </form>
+    </div>
   );
 }
