a9a76d9d567ca0e8edf63531912af89eb68fe34f
jhdcruz <jhdcruz@proton.me>
2024-09-10 21:35:17 +0800

    fix: progress bar not showing when scrolled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
         >
           <DatesProvider settings={{ timezone: 'UTC' }}>
             <ProgressBarProvider>
-              <ProgressBar className="absolute top-0 z-[500] h-1 bg-[--mantine-primary-color-filled] shadow-lg shadow-[--mantine-primary-color-hover]" />
+              <ProgressBar className="fixed left-0 top-0 z-[500] h-1 bg-[--mantine-primary-color-filled] shadow-lg shadow-[--mantine-primary-color-hover]" />
               <ModalsProvider>
                 <Notifications limit={8} />
                 {/* Actual content */}
