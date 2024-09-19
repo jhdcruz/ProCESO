ecbcafad3df41edcd10696b49ac7703a26aba7d1
jhdcruz <jhdcruz@proton.me>
2024-09-20 00:58:44 +0800

    style(compoents): force `PageLoader` to center

--- src/components/Loader/PageLoader.tsx
+++ src/components/Loader/PageLoader.tsx
@@ -2,7 +2,7 @@ import { Paper, Stack, Text } from '@mantine/core';
 import { IconFidgetSpinner } from '@tabler/icons-react';
 
 export const PageLoader = () => (
-  <Paper className="flex size-full items-center self-center py-10 align-middle">
+  <Paper className="flex size-full place-content-center content-center items-center self-center py-10 align-middle">
     <Stack className="mx-auto size-full text-center">
       <IconFidgetSpinner
         className="var(--mantine-color-dimmed) mx-auto block h-10 w-10 animate-spin"
