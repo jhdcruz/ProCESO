c95703c4ed1f18249fd85c79aaf8073c667394e0
jhdcruz <jhdcruz@proton.me>
2024-10-23 01:12:57 +0800

    chore(config): migrate eslint to `v9`

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'react/jsx-sort-props': 'warn',
+      'react/sort-default-props': 'warn',
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
